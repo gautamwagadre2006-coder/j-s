c7604b3910f753ecee44a8da244cdd5bfc3243d4
Gautam wagadre <gautamwagadre2006@gmail.com>
2026-05-11 00:39:22 +0530

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,10 @@
+let score="33";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+// the Number() function is used to convert a string to a number in JavaScript.
+// if the string cannot be converted to a number, it will return NaN (Not a Number).
+// if the string is empty, it will return 0.
+// if the string contains only whitespace, it will also return 0.
---
+++ operation.js
@@ -0,0 +1,14 @@
+let value=3;
+let negValue=-value;
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+
+let str1="hello";
+let str2="world";
+
+let str3=str1+str2;
+console.log(str3);
+console.log("hello"+" "+"world");
